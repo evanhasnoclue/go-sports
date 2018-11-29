7512ad2fe59db9e26d92b44e4654a294c13952a9
BorisFeng <boris.feng@decathlon.com>
2018-11-28 23:40:01 -0800

    filter and search done

--- pages/list/list.js
+++ pages/list/list.js
@@ -6,29 +6,76 @@ Page({
     sports: [],
     inputShowed: false,
     inputVal: "",
+    date: "Date",
+    region: ["Province","City","District"],
+    levels: ["all", "junior", "middle","expert"],
+    customRegion: "All",
+    customDate: "All",
+  },
+  setQuery: function () {
+    const page = this;
+    let title = page.data.inputVal || ''; 
+    let level = page.data.levels[page.data.level] || '';
+    let start_time = page.data.date || ''; 
+    if (page.data.date == "Date" || page.data.date == "ALL" ) {start_time = ""};
+    let province = page.data.province || ''; 
+    if (page.data.province == "All") { province = "" };
+    let city = page.data.city || ''; 
+    if (page.data.city == "All") { city = "" };
+    let district = page.data.district || ''; 
+    if (page.data.district == "All") { district = "" };
+    let query = `query[title]=${title}&query[level]=${level}&query[start_time]=${start_time}&query[province]=${province}&query[city]=${city}&query[district]=${district}`;
+    page.setData({query})
+  },
+
+  getQuery: function () {
+    let page = this;
+    page.setQuery();
+    wx.request({
+      url: 'http://localhost:3000/api/v1/query?' + page.data.query,
+      success(res) {
+        console.log(123, res)
+        page.setData({ sports: res.data.sports })
+      }
+    })
   },
 
   showInput: function () {
     this.setData({
       inputShowed: true
     });
+
   },
   hideInput: function () {
-    this.setData({
+    let page = this;
+    page.setData({
       inputVal: "",
       inputShowed: false
     });
+    page.getQuery()
+    // wx.request({
+    //   // url: app.globalData.url + page.data.inputVal,
+    //   url: 'http://localhost:3000/api/v1/query?query=' + page.data.inputVal,
+    //   success(res) {
+    //     console.log(123, res)
+    //     page.setData({ sports: res.data.sports })
+    //   }
+    // })
   },
   clearInput: function () {
-    this.setData({
+    let page = this;
+    page.setData({
       inputVal: ""
     });
   },
   inputTyping: function (e) {
-    this.setData({
+    let page = this
+    page.setData({
       inputVal: e.detail.value
     });
+    page.getQuery();
   },
+
   /**
    * Lifecycle function--Called when page load
    */
@@ -75,6 +122,52 @@ Page({
     wx.navigateTo({
       url: `../show/show?id=${e.target.dataset.id}`,
     });
+  },
+
+  bindDateChange: function (e) {
+    let page = this;
+    console.log('picker发送选择改变，携带值为', e.detail.value);
+    page.setData({
+      date: e.detail.value
+    });
+    page.getQuery();
+    // wx.request({
+    //   // url: app.globalData.url + page.data.inputVal,
+    //   url: 'http://localhost:3000/api/v1/query?query=' + page.data.date,
+    //   success(res) {
+    //     console.log(123, res)
+    //     page.setData({ sports: res.data.sports })
+    //   }
+    // })
+  },
+  bindRegionChange: function (e) {
+    let page = this;
+    console.log('picker发送选择改变，携带值为', e.detail.value);
+    page.setData({
+      region: e.detail.value,
+      province: e.detail.value[0],
+      city: e.detail.value[1],
+      district: e.detail.value[2],
+    });
+    page.getQuery()
+  },
+  bindPickerChange: function (e) {
+    let page = this;
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    page.setData({
+      level: e.detail.value
+    });
+    page.getQuery()
+    // page.setQuery()
+    // wx.request({
+    //   // url: app.globalData.url + page.data.inputVal,
+    //   url: 'http://localhost:3000/api/v1/query?' + page.data.query,
+    //   // url: 'http://localhost:3000/api/v1/query?query=' + page.data.levels[page.data.level],
+    //   success(res) {
+    //     console.log(123, res)
+    //     page.setData({ sports: res.data.sports })
+    //   }
+    // })
   },
 
   /**
