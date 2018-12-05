50beae91a0bca185e9394538cbd3bf23e060a3ac
BorisFeng <boris.feng@decathlon.com>
2018-12-04 23:37:54 -0800

    chart done!

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -8,29 +8,12 @@ var wxCharts = require('../../utils/wxcharts.js');
 var radarChart = null;
 var columnChart = null;
 
-var chartData = {
+let chartData = {
   main: {
     title: '总成交量',
     data: [5, 2, 4, 3, 4],
     categories: ['swimming', 'dancing', 'badminton', 'walking', 'running']
-  },
-  sub: [{
-    title: '2012年度成交量',
-    data: [70, 40, 65, 100, 34, 18],
-    categories: ['1', '2', '3', '4', '5', '6']
-  }, {
-    title: '2013年度成交量',
-    data: [55, 30, 45, 36, 56, 13],
-    categories: ['1', '2', '3', '4', '5', '6']
-  }, {
-    title: '2014年度成交量',
-    data: [76, 45, 32, 74, 54, 35],
-    categories: ['1', '2', '3', '4', '5', '6']
-  }, {
-    title: '2015年度成交量',
-    data: [76, 54, 23, 12, 45, 65],
-    categories: ['1', '2', '3', '4', '5', '6']
-  }]
+  }
 };
 
 Page({
@@ -81,44 +64,46 @@ Page({
     sliderLeft: 0,
 
   },
-  backToMainChart: function () {
-    this.setData({
-      chartTitle: chartData.main.title,
-      isMainChartDisplay: true
-    });
-    columnChart.updateData({
-      categories: chartData.main.categories,
-      series: [{
-        name: '成交量',
-        data: chartData.main.data,
-        format: function (val, name) {
-          return val.toFixed(2) + '次';
-        }
-      }]
-    });
-  },
+  // backToMainChart: function () {
+  //   this.setData({
+  //     chartTitle: chartData.main.title,
+  //     isMainChartDisplay: true
+  //   });
+  //   columnChart.updateData({
+  //     categories: chartData.main.categories,
+  //     series: [{
+  //       name: '成交量',
+  //       data: chartData.main.data,
+  //       format: function (val, name) {
+  //         return val.toFixed(2) + '次';
+  //       }
+  //     }]
+  //   });
+  // },
   touchHandler: function (e) {
-    var index = columnChart.getCurrentDataIndex(e);
-    if (index > -1 && index < chartData.sub.length && this.data.isMainChartDisplay) {
-      this.setData({
-        chartTitle: chartData.sub[index].title,
-        isMainChartDisplay: false
-      });
-      columnChart.updateData({
-        categories: chartData.sub[index].categories,
-        series: [{
-          name: '成交量',
-          data: chartData.sub[index].data,
-          format: function (val, name) {
-            return val.toFixed(0) + 'times';
-          }
-        }]
-      });
+    // var index = columnChart.getCurrentDataIndex(e);
+    // console.log(9, this.data)
+    // if (index > -1 && index < chartData.sub.length && this.data.isMainChartDisplay) {
+    //   this.setData({
+    //     chartTitle: chartData.sub[index].title,
+    //     isMainChartDisplay: false
+    //   });
+    //   columnChart.updateData({
+    //     categories: chartData.sub[index].categories,
+    //     series: [{
+    //       name: '成交量',
+    //       data: chartData.sub[index].data,
+    //       format: function (val, name) {
+    //         return val.toFixed(0) + 'times';
+    //       }
+    //     }]
+    //   });
 
-    }
+    // }
   },
   touchHandler: function (e) {
-    console.log(radarChart.getCurrentDataIndex(e));
+    // console.log('10')
+    // console.log(radarChart.getCurrentDataIndex(e));
 
   },
 
@@ -166,6 +151,8 @@ Page({
   onLoad: function (options) {
     var that = this;
     let likes = 0;
+    let times = [];
+    let categories = [];
     wx.getStorage({
       key: 'current_user',
       success: (res) => {
@@ -182,11 +169,19 @@ Page({
               res.data.gender = "man"
             }
             res.data.sports.forEach (sport => likes += sport.like)
+            res.data.fav_sports.forEach( (sport) => {
+              if (sport[0] !== ""){
+              times.push(sport[1])
+              categories.push(sport[0])
+              }
+            })
             // Update local data
             that.setData({
               profile: res.data,
               unread: app.globalData.unread,
-              likes: likes
+              likes: likes,
+              times:times,
+              categories:categories
             });
         // console.log(that.data )
         let result = 0
@@ -278,6 +273,11 @@ wx.navigateTo({
   },
 
   onReady: function(e) {
+    let page = this;
+    let times=[];
+    let categories =[];
+    // console.log(1, data)
+    // console.log(2, data.categories)
     var windowWidth = 320;
     try {
       var res = wx.getSystemInfoSync();
@@ -286,37 +286,92 @@ wx.navigateTo({
       console.error('getSystemInfoSync failed!');
     }
 
-    columnChart = new wxCharts({
-      canvasId: 'columnCanvas',
-      type: 'column',
-      animation: true,
-      categories: chartData.main.categories,
-      series: [{
-        name: 'DIVERSIFIED SPORTS',
-        data: chartData.main.data,
-        format: function (val, name) {
-          return val.toFixed(0) + '';
-        }
-      }],
-      yAxis: {
-        format: function (val) {
-          return val + 'times';
-        },
-        title: 'Frequency',
-        min: 0
-      },
-      xAxis: {
-        disableGrid: false,
-        type: 'calibration'
-      },
-      extra: {
-        column: {
-          width: 15
-        }
-      },
-      width: windowWidth,
-      height: 200,
-    });
+    wx.getStorage({
+      key: 'current_user',
+      success: (res) => {
+        console.log(8,res)
+        const profile = res.data;
+        wx.request({
+          url: app.globalData.url + "/users/" + profile.id,
+          method: 'GET',
+          success(res) {
+            res.data.fav_sports.forEach((sport) => {
+              if (sport[0] !== "") {
+                times.push(sport[1])
+                categories.push(sport[0])
+              }
+            })
+            columnChart = new wxCharts({
+              canvasId: 'columnCanvas',
+              type: 'column',
+              animation: true,
+              // categories: data.categories,
+              categories: categories,
+              series: [{
+                name: 'DIVERSIFIED SPORTS',
+                data: times,
+                format: function (val, name) {
+                  return val.toFixed(0) + '';
+                }
+              }],
+              yAxis: {
+                format: function (val) {
+                  return val + 'times';
+                },
+                title: 'Frequency',
+                min: 0
+              },
+              xAxis: {
+                disableGrid: false,
+                type: 'calibration'
+              },
+              extra: {
+                column: {
+                  width: 15
+                }
+              },
+              width: windowWidth,
+              height: 200,
+            });
+
+
+          }
+        })
+      }
+    })
+   
+    // columnChart = new wxCharts({
+    //   canvasId: 'columnCanvas',
+    //   type: 'column',
+    //   animation: true,
+    //   // categories: data.categories,
+    //   categories: categories,
+    //   series: [{
+    //     name: 'DIVERSIFIED SPORTS',
+    //     data: chartData.main.data,
+    //     format: function (val, name) {
+    //       return val.toFixed(0) + '';
+    //     }
+    //   }],
+    //   yAxis: {
+    //     format: function (val) {
+    //       return val + 'times';
+    //     },
+    //     title: 'Frequency',
+    //     min: 0
+    //   },
+    //   xAxis: {
+    //     disableGrid: false,
+    //     type: 'calibration'
+    //   },
+    //   extra: {
+    //     column: {
+    //       width: 15
+    //     }
+    //   },
+    //   width: windowWidth,
+    //   height: 200,
+    // });
 
   },
 
